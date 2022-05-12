38821a8b6913a67f5abaf7210ce6d3795cc7f219
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-05-12 21:38:15 +0000

    initial commit

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://rpncoibdjdivii:4acd5aec58463fea7cb73fa3525d2218bbece6c6bc7641bc29fdf623455d58fd@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d82mnkh45hr95g"
+os.environ["SECRET_KEY"] = "randomkey!!2022@"
